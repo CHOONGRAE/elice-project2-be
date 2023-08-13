46a45e0f548fcce1a0938c0f6ed574a910e184b4
CHOONGRAE <choongraej@gmail.com>
2023-08-13 21:42:34 +0900

    Feat: prisma 기본 연동

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { setupSwagger } from './utils/swagger';
+import { PrismaService } from './prisma/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const prismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHooks(app);
 
   setupSwagger(app);
 
