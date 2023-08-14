4024b1c46435c437ff7272efb35e94a0b0c7ec2b
CHOONGRAE <choongraej@gmail.com>
2023-08-15 08:16:20 +0900

    Feat: create-auth dto 타입 설정

--- src/auth/dto/create-auth.dto.ts
+++ src/auth/dto/create-auth.dto.ts
@@ -1,10 +1,22 @@
-import { ApiProperty, OmitType } from '@nestjs/swagger';
-import { AuthEntity } from '../entities';
+import { ApiProperty, IntersectionType, OmitType } from '@nestjs/swagger';
+import { AuthEntity, UserEntity } from '../entities';
 
-export class CreateAuthDto extends OmitType(AuthEntity, ['id']) {
+export class CreateAuthDto extends IntersectionType(
+  OmitType(AuthEntity, ['id'] as const),
+  OmitType(UserEntity, ['userId', 'selectedAnswerCount', 'createdAt'] as const),
+) {
   @ApiProperty()
   email: string;
 
   @ApiProperty()
   password: string;
+
+  @ApiProperty()
+  userName: string;
+
+  @ApiProperty()
+  birthDate: string;
+
+  @ApiProperty()
+  phoneNumber: string;
 }
