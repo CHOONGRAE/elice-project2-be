85cc0c12e024a92d4e642651119c4fdcef509d41
CHOONGRAE <choongraej@gmail.com>
2023-08-14 16:59:55 +0900

    Feat: mailer module 초기화

--- src/mailer/mailer.module.ts
+++ src/mailer/mailer.module.ts
@@ -1,5 +1,39 @@
 import { Global, Module } from '@nestjs/common';
+import { MailerModule as MailModule } from '@nestjs-modules/mailer';
+import { MailerService } from './mailer.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { EjsAdapter } from '@nestjs-modules/mailer/dist/adapters/ejs.adapter';
 
 @Global()
-@Module({})
+@Module({
+  imports: [
+    MailModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        transport: {
+          host: configService.get('MAILER_HOST'),
+          port: 587,
+          auth: {
+            user: configService.get('MAILER_EMAIL'),
+            pass: configService.get('MAILER_PASSWORD'),
+          },
+        },
+        defaults: {
+          from: `"no-reply" ${configService.get('MAILER_EMAIL')}`,
+        },
+        preview: true,
+        template: {
+          dir: __dirname + '/templates',
+          adapter: new EjsAdapter(),
+          options: {
+            strict: true,
+          },
+        },
+      }),
+    }),
+  ],
+  providers: [MailerService],
+  exports: [MailerService],
+})
 export class MailerModule {}
