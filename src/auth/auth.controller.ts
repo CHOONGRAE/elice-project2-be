c67ed70a7055bab60b92f7f330f436e361e86970
CHOONGRAE <choongraej@gmail.com>
2023-08-14 16:31:36 +0900

    Feat: validate email controller 초안

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -9,35 +9,19 @@ import {
   Query,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { ValidateEmailDto } from './dto/validate-email.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Get('authenticate')
-  async authenticateEmail(@Query('email') email: string) {
-    return await this.authService.authenticateEmail(email);
+  @Get('validate')
+  async validateEmail(@Query('email') email: string) {
+    return await this.authService.validateEmail(email);
   }
 
-  @Get()
-  findAll() {
-    return this.authService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.authService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
-    return this.authService.update(+id, updateAuthDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.authService.remove(+id);
+  @Post('validate')
+  async validate(@Body() validateEmail: ValidateEmailDto) {
+    return await this.authService.validate(validateEmail);
   }
 }
