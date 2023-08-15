a0f07b811141937f2c85855c3eecde9398053649
CHOONGRAE <choongraej@gmail.com>
2023-08-16 05:46:48 +0900

    Feat: swagger에서 cookie사용 설정

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: [process.env.HOST],
+    origin: true,
     credentials: true,
   });
   app.use(cookieParser());
