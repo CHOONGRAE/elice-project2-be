92f0fe6474af3e540f6b56df83cb54301943d991
CHOONGRAE <choongraej@gmail.com>
2023-08-15 09:34:01 +0900

    Feat: dto에 class validator 적용

--- src/auth/dto/verificate-code.dto.ts
+++ src/auth/dto/verificate-code.dto.ts
@@ -1,12 +1,17 @@
 import { ApiProperty, PickType } from '@nestjs/swagger';
 import { AuthEntity } from '../entities';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class VerificateCodeDto extends PickType(AuthEntity, [
   'email',
 ] as const) {
+  @IsNotEmpty()
+  @IsString()
   @ApiProperty()
   email: string;
 
+  @IsNotEmpty()
+  @IsString()
   @ApiProperty()
   code: string;
 }
