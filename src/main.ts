26e341dea875781278b43b59a954dddabb6960a0
CHOONGRAE <choongraej@gmail.com>
2023-08-17 10:20:17 +0900

    Feat: cors 설정

--- src/main.ts
+++ src/main.ts
@@ -17,8 +17,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: true,
+    origin: 'http://localhost:3000',
     credentials: true,
+    exposedHeaders: ['Authorization'],
+    allowedHeaders: ['Origin', 'X-Request-With', 'Content-Type', 'Accept'],
   });
   app.use(cookieParser());
 
