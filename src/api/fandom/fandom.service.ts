d88ac506794206a44d76e2e2b71783e8b4567aeb
CHOONGRAE <choongraej@gmail.com>
2023-08-21 04:09:36 +0900

    Feat: fandom service 추가
    - 초안 작업중
    - create,update dto 추가
    - create, update, delete 서비스 로직 추가
    - fandomList 로직 작업중

---
+++ src/api/fandom/fandom.service.ts
@@ -0,0 +1,94 @@
+import { CreateFandomDto } from '@dto/fandomDto/create-fandom.dto';
+import { UpdateFandomDto } from '@dto/fandomDto/update-fandom.dto';
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '@prisma/prisma.service';
+
+@Injectable()
+export class FandomService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async createFandom(createFandomDto: CreateFandomDto) {
+    await this.prisma.fandoms.create({
+      data: createFandomDto,
+    });
+  }
+
+  async updateFandom(fandomId: number, updateFandomDto: UpdateFandomDto) {
+    await this.prisma.fandoms.update({
+      where: {
+        id: fandomId,
+      },
+      data: updateFandomDto,
+    });
+  }
+
+  async deleteFandom(fandomId: number) {
+    await this.prisma.fandoms.delete({
+      where: {
+        id: fandomId,
+      },
+    });
+  }
+
+  async getFandoms() {
+    return { data: await this.makeSortedFandoms() };
+  }
+
+  async getHotFandoms() {
+    return { data: await this.makeSortedFandoms() };
+  }
+
+  async getFandomsByUser() {
+    return 'test';
+  }
+
+  async getFandomsBySearch() {
+    return 'test';
+  }
+
+  private async makeSortedFandoms() {
+    const result = await this.prisma.fandoms.findMany({
+      include: {
+        _count: {
+          select: { subscribes: true },
+        },
+        messages: {
+          take: 1,
+          orderBy: {
+            createdAt: 'desc',
+          },
+          select: {
+            createdAt: true,
+          },
+        },
+      },
+      take: 4,
+      orderBy: {
+        rank: {
+          point: 'desc',
+        },
+        createdAt: 'asc',
+      },
+    });
+
+    return result.map(
+      (
+        {
+          id,
+          fandomName,
+          thumbnailImgUrl,
+          _count: { subscribes: memberLength },
+          messages: [{ createdAt: lastChatTime }],
+        },
+        i,
+      ) => ({
+        id,
+        rank: i,
+        fandomName,
+        thumbnailImgUrl,
+        memberLength,
+        lastChatTime,
+      }),
+    );
+  }
+}
