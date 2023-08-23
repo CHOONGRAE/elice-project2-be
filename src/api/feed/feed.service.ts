dc8ed835c9c10a5ee0d01e00fbf46f974917c620
CHOONGRAE <choongraej@gmail.com>
2023-08-24 06:00:01 +0900

    Feat: feed service 초안

---
+++ src/api/feed/feed.service.ts
@@ -0,0 +1,154 @@
+import { HashTagService } from '@api/hash-tag/hash-tag.service';
+import { SonminsuItemService } from '@api/sonminsu-item/sonminsu-item.service';
+import { CreateFeedDto } from '@dto/feedDto/create-feed.dto';
+import { PaginateFeedDto } from '@dto/feedDto/paginate-feed.dto';
+import { UpdateFeedDto } from '@dto/feedDto/update-feed.dto';
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '@prisma/prisma.service';
+import { S3Service } from 'src/s3/s3.service';
+
+@Injectable()
+export class FeedService {
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly s3: S3Service,
+    private readonly sonminsuItem: SonminsuItemService,
+    private readonly hashTag: HashTagService,
+  ) {}
+
+  async createFeed(userId: number, createFeedDto: CreateFeedDto) {
+    const { fandomId, content, hashTags, image, sonminsuItems } = createFeedDto;
+    const createdFeed = await this.prisma.feeds.create({
+      include: {
+        fandoms: {
+          where: { id: fandomId },
+        },
+        // 조인 처리
+      },
+      data: {
+        userId,
+        fandomId,
+        content,
+        images: {
+          create: [
+            {
+              url: (await this.s3.uploadImage(image)) as string,
+            },
+          ],
+        },
+        tags: {
+          create: await this.createHashTags(hashTags),
+        },
+      },
+    });
+
+    await Promise.all(
+      sonminsuItems.map((id) =>
+        this.sonminsuItem.updateSonminsuItem(id, {
+          feedId: createdFeed.id,
+          registration: true,
+        }),
+      ),
+    );
+
+    return createdFeed;
+  }
+
+  async updateFeed(updateFeedDto: UpdateFeedDto) {
+    const { feedId, content, hashTags, sonminsuItems } = updateFeedDto;
+
+    const updatedFeed = await this.prisma.feeds.update({
+      where: {
+        id: feedId,
+      },
+      data: {
+        content,
+        tags: {
+          create: await this.createHashTags(hashTags),
+        },
+      },
+    });
+
+    await Promise.all(
+      sonminsuItems.map((id) =>
+        this.sonminsuItem.updateSonminsuItem(id, {
+          feedId: feedId,
+          registration: true,
+        }),
+      ),
+    );
+
+    return updatedFeed;
+  }
+
+  async deleteFeed(feedId: number) {
+    const deletedFeed = await this.prisma.feeds.delete({
+      where: {
+        id: feedId,
+      },
+    });
+
+    return deletedFeed;
+  }
+
+  async getFeeds(pagination: PaginateFeedDto) {
+    const { page, perPage } = pagination;
+
+    const result = await this.prisma.feeds.findMany({
+      skip: perPage * (page - 1),
+      take: perPage,
+    });
+
+    return result;
+  }
+
+  async getFeedsByUser(userId: number, pagination: PaginateFeedDto) {
+    const { page, perPage } = pagination;
+
+    const result = await this.prisma.feeds.findMany({
+      where: {
+        userId,
+      },
+      skip: perPage * (page - 1),
+      take: perPage,
+    });
+
+    return result;
+  }
+
+  async getFeedsByFandom(fandomId: number, pagination: PaginateFeedDto) {
+    const { page, perPage } = pagination;
+
+    const result = await this.prisma.feeds.findMany({
+      where: {
+        fandomId,
+      },
+      skip: perPage * (page - 1),
+      take: perPage,
+    });
+
+    return result;
+  }
+
+  async getFeedById(id: number) {
+    const feed = await this.prisma.feeds.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    return feed;
+  }
+
+  private async createHashTags(hashTags: string[]) {
+    return (
+      await this.hashTag.createHashTags(
+        hashTags.map((tag) => ({
+          tag,
+        })),
+      )
+    ).map((id) => ({
+      hashTagId: id,
+    }));
+  }
+}
