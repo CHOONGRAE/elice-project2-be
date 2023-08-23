404d7def17ef32ae1443a7eaf8e281d7307059fe
CHOONGRAE <choongraej@gmail.com>
2023-08-24 08:53:52 +0900

    Feat: 스키마 변경에 의한 오류 수정

--- src/api/auth/auth.module.ts
+++ src/api/auth/auth.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { S3Module } from '@s3';
 
 @Module({
+  imports: [S3Module],
   controllers: [AuthController],
   providers: [AuthService],
 })
