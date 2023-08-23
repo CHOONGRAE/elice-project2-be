35c3fa4b34031704af3c1c4a7566d50db0c35f1f
CHOONGRAE <choongraej@gmail.com>
2023-08-24 05:59:38 +0900

    Feat: DTO들 생성

---
+++ src/dto/feedDto/paginate-feed.dto.ts
@@ -0,0 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber } from 'class-validator';
+
+export class PaginateFeedDto {
+  @ApiProperty({ default: 1, required: false })
+  @IsNumber()
+  page: number;
+
+  @ApiProperty({ default: 10, required: false })
+  @IsNumber()
+  perPage: number;
+}
