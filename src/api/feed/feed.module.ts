9c880aacfaadd4feb2ff75ce46004557ac8a3327
CHOONGRAE <choongraej@gmail.com>
2023-08-24 05:44:44 +0900

    Feat: feed module 생성

--- src/api/feed/feed.module.ts
+++ src/api/feed/feed.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { FeedController } from './feed.controller';
 import { FeedService } from './feed.service';
+import { S3Module } from '@s3';
+import { SonminsuItemModule } from '@api/sonminsu-item/sonminsu-item.module';
+import { HashTagModule } from '@api/hash-tag/hash-tag.module';
 
 @Module({
+  imports: [S3Module, SonminsuItemModule, HashTagModule],
   controllers: [FeedController],
   providers: [FeedService],
   exports: [FeedService],
