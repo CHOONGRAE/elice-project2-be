b25e56c51e4e94776c1fd4fb5941da62172acbf5
CHOONGRAE <choongraej@gmail.com>
2023-08-24 09:22:34 +0900

    Feat: sonminsuRequestBookmark DTO 생성

---
+++ src/dto/sonminsuRequestBookmarkDto/sonminsuRequestBookmark.dto.ts
@@ -0,0 +1,13 @@
+import { SonminsuRequestBookmarkEntity } from '@entities';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber } from 'class-validator';
+
+export class SonminsuRequestBookmarkDto extends SonminsuRequestBookmarkEntity {
+  @IsNumber()
+  @ApiProperty()
+  userId: number;
+
+  @IsNumber()
+  @ApiProperty()
+  fandomId: number;
+}
