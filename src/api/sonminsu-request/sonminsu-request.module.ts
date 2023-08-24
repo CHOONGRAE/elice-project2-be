42f30ef2892c0a35151a686180689013c29ffae4
CHOONGRAE <choongraej@gmail.com>
2023-08-25 05:58:02 +0900

    Feat: s3 module import

--- src/api/sonminsu-request/sonminsu-request.module.ts
+++ src/api/sonminsu-request/sonminsu-request.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SonminsuRequestService } from './sonminsu-request.service';
 import { SonminsuRequestController } from './sonminsu-request.controller';
+import { S3Module } from '@s3';
 
 @Module({
+  imports: [S3Module],
   providers: [SonminsuRequestService],
   controllers: [SonminsuRequestController],
   exports: [SonminsuRequestService],
