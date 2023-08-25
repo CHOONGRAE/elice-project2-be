f71d49c25057cc1bf30695846a347f89ebe9f91a
CHOONGRAE <choongraej@gmail.com>
2023-08-25 17:17:20 +0900

    Feat: user모듈에 sonminsuRequest 주입

--- src/api/user/user.module.ts
+++ src/api/user/user.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { FandomModule } from '@api/fandom/fandom.module';
+import { SonminsuRequestModule } from '@api/sonminsu-request/sonminsu-request.module';
 
 @Module({
-  imports: [FandomModule],
+  imports: [FandomModule, SonminsuRequestModule],
   providers: [UserService],
   controllers: [UserController],
 })
