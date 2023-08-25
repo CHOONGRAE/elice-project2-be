039842a80232965f512b98ed402c766cc857f86c
CHOONGRAE <choongraej@gmail.com>
2023-08-25 09:15:50 +0900

    Feat: 퍼블릭 하지 못한 엔드 포인트 삭제

--- src/api/sonminsu-item/sonminsu-item.controller.ts
+++ src/api/sonminsu-item/sonminsu-item.controller.ts
@@ -1,20 +1,7 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  Patch,
-  Post,
-  Query,
-  UseGuards,
-} from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { SonminsuItemService } from './sonminsu-item.service';
-import { ApiParam, ApiQuery, ApiTags } from '@nestjs/swagger';
-import { CreateSonminsuItemDto } from '@dto/sonminsuItemDto/create-sonminsuItem.dto';
-import { AuthGuard } from '@guards/auth.guard';
-import { UpdateSonminsuItemDto } from '@dto/sonminsuItemDto/update-sonminsuItem.dto';
 import { PaginateSonminsuItemDto } from '@dto/sonminsuItemDto/paginate-sonminsuItem.dto';
+import { ApiTags } from '@nestjs/swagger';
 
 @Controller({
   path: 'sonminsu-items',
@@ -27,30 +14,5 @@ export class SonminsuItemController {
   @Get()
   async getSonminsuItems(@Query() pagination: PaginateSonminsuItemDto) {
     return await this.sonminsuItemService.getSonminsuItems(pagination);
-  }
-
-  @Post()
-  @UseGuards(AuthGuard)
-  async createSonminsuItem(
-    @Body() createSonminsuItemDto: CreateSonminsuItemDto,
-  ) {
-    return await this.sonminsuItemService.createSonminsuItem(
-      createSonminsuItemDto,
-    );
-  }
-
-  @Patch(':id')
-  @UseGuards(AuthGuard)
-  @ApiParam({
-    name: 'id',
-  })
-  async updateSonminsuItem(
-    @Param('id') id: number,
-    updateSonminsuItemDto: UpdateSonminsuItemDto,
-  ) {
-    return await this.sonminsuItemService.updateSonminsuItem(
-      id,
-      updateSonminsuItemDto,
-    );
   }
 }
