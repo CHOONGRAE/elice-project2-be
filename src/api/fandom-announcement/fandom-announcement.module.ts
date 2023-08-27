fe30e7250770f5dd1d9299de274edeb49cfe2302
CHOONGRAE <choongraej@gmail.com>
2023-08-28 02:48:31 +0900

    Feat: 공지 가져오기, 공지 작성 로직 수정

--- src/api/fandom-announcement/fandom-announcement.module.ts
+++ src/api/fandom-announcement/fandom-announcement.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { FandomAnnouncementService } from './fandom-announcement.service';
+import { FandomAnnouncementController } from './fandom-announcement.controller';
 
 @Module({
   providers: [FandomAnnouncementService],
   exports: [FandomAnnouncementService],
+  controllers: [FandomAnnouncementController],
 })
 export class FandomAnnouncementModule {}
