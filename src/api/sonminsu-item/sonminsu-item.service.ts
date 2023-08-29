c42922414e7d4870ac584946404b7dcd2f10aa55
CHOONGRAE <choongraej@gmail.com>
2023-08-30 03:39:02 +0900

    Feat: 손민수 아이템 다듬기

--- src/api/sonminsu-item/sonminsu-item.service.ts
+++ src/api/sonminsu-item/sonminsu-item.service.ts
@@ -5,6 +5,7 @@ import { UpdateSonminsuItemDto } from '@dto/sonminsuItemDto/update-sonminsuItem.
 import { Injectable } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
 import { Prisma } from '@prisma/client';
+import { DefaultArgs } from '@prisma/client/runtime/library';
 import { PrismaService } from '@prisma/prisma.service';
 import { ScraperService } from 'src/scraper/scraper.service';
 
@@ -34,7 +35,7 @@ export class SonminsuItemService {
       select: this.selectField,
     });
 
-    return { data: result };
+    return { data: this.transformData(result) };
   }
 
   async updateSonminsuItem(
@@ -47,7 +48,7 @@ export class SonminsuItemService {
       select: this.selectField,
     });
 
-    return { data: result };
+    return { data: this.transformData(result) };
   }
 
   @Cron(CronExpression.EVERY_2_HOURS)
@@ -111,7 +112,7 @@ export class SonminsuItemService {
     });
 
     return {
-      data: result,
+      data: result.map(this.transformData),
       totalPage: Math.ceil(totalCount / perPage),
       currentPage: page,
     };
@@ -186,6 +187,38 @@ export class SonminsuItemService {
                 contains: search,
               },
             },
+            {
+              answer: {
+                request: {
+                  groupName: {
+                    contains: search,
+                  },
+                },
+              },
+            },
+            {
+              answer: {
+                request: {
+                  artistName: {
+                    contains: search,
+                  },
+                },
+              },
+            },
+            {
+              feed: {
+                groupName: {
+                  contains: search,
+                },
+              },
+            },
+            {
+              feed: {
+                artistName: {
+                  contains: search,
+                },
+              },
+            },
           ],
         },
         {
@@ -220,7 +253,7 @@ export class SonminsuItemService {
     });
 
     return {
-      data: result,
+      data: result.map(this.transformData),
       totalPage: Math.ceil(totalCount / perPage),
       currentPage: page,
     };
@@ -234,16 +267,39 @@ export class SonminsuItemService {
       select: this.selectField,
     });
 
-    return { data: item };
+    return { data: this.transformData(item) };
   }
+
+  private readonly transformData = ({ feed, answer, ...data }) => ({
+    ...data,
+    groupName: data.groupName || feed?.groupName || answer?.request?.groupName,
+    artistName:
+      data.artistName || feed?.artistName || answer?.request?.artistName,
+  });
 
   private readonly selectField = {
     id: true,
     originUrl: true,
+    imgUrl: true,
     title: true,
     price: true,
-    imgUrl: true,
     groupName: true,
     artistName: true,
+    feed: {
+      select: {
+        groupName: true,
+        artistName: true,
+      },
+    },
+    answer: {
+      select: {
+        request: {
+          select: {
+            groupName: true,
+            artistName: true,
+          },
+        },
+      },
+    },
   };
 }
