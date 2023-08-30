a47a75b8add594c0c43353bdbba08ae8a06950f5
CHOONGRAE <choongraej@gmail.com>
2023-08-31 01:43:49 +0900

    Feat: 필요한 서비스들 초안 작성중

--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '@prisma/prisma.service';
 
 @Injectable()
-export class ChatService {}
+export class ChatService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async getRoomsForUser(userId: number) {
+    const rooms = await this.prisma.subscribes.findMany({
+      where: {
+        userId,
+      },
+      select: {
+        fandom: {
+          select: {
+            id: true,
+            fandomName: true,
+            image: true,
+          },
+        },
+      },
+    });
+
+    return rooms.map(({ fandom }) => fandom);
+  }
+}
