53a3b2e3e033c99208648cd56a8f76624bc11e51
CHOONGRAE <choongraej@gmail.com>
2023-08-31 13:23:42 +0900

    Feat: 감옥보내기, 대화 수신, 전송

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -3,8 +3,10 @@ import { ChatService } from './chat.service';
 import { ChatGateway } from './chat.gateway';
 import { MessageService } from './message.service';
 import { UserService } from './user.service';
+import { RedisModule } from '@redis/redis.module';
 
 @Module({
+  imports: [RedisModule],
   providers: [ChatService, MessageService, UserService, ChatGateway],
 })
 export class ChatModule {}
